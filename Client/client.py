1c51a2b9dbb7076f1cf23896e7fbd48fb781d388
Unknown <Jkonechy@live.com>
2021-11-09 16:04:56 -0800

    Tried to make a list_file function
    Didn't have time to test the function in Linux, but it compiled so ill test it later.

--- Client/client.py
+++ Client/client.py
@@ -11,12 +11,51 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def conn():
     # Connect to the server
-    print ("Sending server request...")
+    print("Sending server request...")
     try:
         s.connect((TCP_IP, TCP_PORT))
         print ("Connection sucessful")
     except:
         print ("Connection unsucessful. Make sure the server is online.")
+
+
+def list_files():
+    # List the files avaliable on the file server
+    # Called list_files(), not list() (as in the format of the others) to avoid the standard python function list()
+    print ("Requesting files...\n")
+    try:
+        # Send list request
+        s.send("LIST")
+    except:
+        print ("Couldn't make server request. Make sure a connection has bene established.")
+        return
+    try:
+        # First get the number of files in the directory
+        number_of_files = struct.unpack("i", s.recv(4))[0]
+        # Then enter into a loop to recieve details of each, one by one
+        for i in range(int(number_of_files)):
+            # Get the file name size first to slightly lessen amount transferred over socket
+            file_name_size = struct.unpack("i", s.recv(4))[0]
+            file_name = s.recv(file_name_size)
+            # Also get the file size for each item in the server
+            file_size = struct.unpack("i", s.recv(4))[0]
+            print ("\t{} - {}b".format(file_name, file_size))
+            # Make sure that the client and server are syncronised
+            s.send("1")
+        # Get total size of directory
+        total_directory_size = struct.unpack("i", s.recv(4))[0]
+        print ("Size: {}b".format(total_directory_size))
+    except:
+        print ("No listing found")
+        return
+    try:
+        # Final check
+        s.send("1")
+        return
+    except:
+        print ("No server confirmation")
+        return
+
 
 print ("\n\nWelcome to the FTP client."
        "\n\nPlease type one of the following functions :" 
--- Server/server.py
+++ Server/server.py
@@ -16,3 +16,28 @@ s.listen(1)
 conn, addr = s.accept()
 
 print ("\nConnected to the server sucessful...".format(addr))
+
+def list_files():
+    print ("Listing files:")
+    # Get list of files in directory
+    listing = os.listdir(os.getcwd())
+    # Send over the number of files, so the client knows what to expect (and avoid some errors)
+    conn.send(struct.pack("i", len(listing)))
+    total_directory_size = 0
+    # Send over the file names and sizes whilst totaling the directory size
+    for i in listing:
+        # File name size
+        conn.send(struct.pack("i", sys.getsizeof(i)))
+        # File name
+        conn.send(i)
+        # File content size
+        conn.send(struct.pack("i", os.path.getsize(i)))
+        total_directory_size += os.path.getsize(i)
+        # Make sure that the client and server are syncronised
+        conn.recv(BUFFER_SIZE)
+    # Sum of file sizes in directory
+    conn.send(struct.pack("i", total_directory_size))
+    #Final check
+    conn.recv(BUFFER_SIZE)
+    print ("The Files have been successfully Listed!")
+    return
